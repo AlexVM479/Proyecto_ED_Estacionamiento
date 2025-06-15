4c89bce12ae3278d271df2f22e3b4207e6bde6cd
AlexVM479 <alexvergaramalpartida46@gmail.com>
2025-06-15 01:26:17 -0500

    ED

---
+++ src/main/java/com/mycompany/proyectoed/ProyectoED.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectoed;
+
+/**
+ *
+ * @author administrador
+ */
+public class ProyectoED {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Worldebvscvnddsk!");
+    }
+}
