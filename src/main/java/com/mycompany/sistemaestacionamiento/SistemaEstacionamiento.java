24d30c1e1e002886dbfd20e66eef048540f168f1
demiannj123 <josep.132.nm@gmail.com>
2025-07-02 21:07:07 -0500

    mensaje

--- src/main/java/com/mycompany/sistemaestacionamiento/SistemaEstacionamiento.java
+++ src/main/java/com/mycompany/sistemaestacionamiento/SistemaEstacionamiento.java
@@ -8,7 +8,7 @@ import com.mycompany.sistemaestacionamiento.gui.LogIn;
 public class SistemaEstacionamiento {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("asdf");
         
         LogIn login = new LogIn();
         login.setVisible(true);
